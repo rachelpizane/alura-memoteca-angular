ae906fee36c877c082f3909ed5c433e7f1494eb3
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-14 18:13:26 -0300

    feat: criado o cabecalho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
