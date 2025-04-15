8f31e4d289c059b3c71dcee8929acc3341d07bd4
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-15 18:44:41 -0300

    feat: incluido a diretiva ngClass

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -10,7 +10,7 @@ export class ListarPensamentosComponent implements OnInit {
   pensamentos: Pensamento[] = [
     { conteudo: 'Pensamento 1', autoria: 'Autor 1', modelo: 'modelo1' },
     { conteudo: 'Pensamento 2', autoria: 'Autor 2', modelo: 'modelo2' },
-    { conteudo: 'Pensamento 3', autoria: 'Autor 3', modelo: 'modelo3' }
+    { conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget consequat magna. Suspendisse potenti. Integer tincidunt, massa quis tincidunt porttitor, nisi risus dictum eros, at pharetra nulla sapien id magna. Duis non pulvinar enim. Etiam imperdiet, nisl non gravida feugiat, lectus nisi laoreet erat, sed feugiat augue mauris sed leo. Aenean vitae odio nec neque faucibus vestibulum. Curabitur tincidunt varius turpis a porta.', autoria: 'Autor 3', modelo: 'modelo3' }
   ];
   exibirPensamentos: boolean = this.pensamentos.length > 0;
 
