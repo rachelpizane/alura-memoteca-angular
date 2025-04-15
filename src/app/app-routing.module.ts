55dad7a8d3e61331c5f3f869fd60c66e26343e98
Rachel Pizane <rachel.maia@dbserver.com.br>
2025-04-15 15:55:04 -0300

    feat: Incluido as rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './components/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentosComponent } from './components/pensamentos/listar-pensamentos/listar-pensamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listar-pensamento',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criar-pensamento',
+    component: CriarPensamentosComponent
+  },
+  {
+    path: 'listar-pensamento',
+    component: ListarPensamentosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
